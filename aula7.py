9f86bfcca6f7d7f541bdfa04100f65d801264610
gigante <guilhermecavalcantefarias@gmail.com>
2024-05-17 16:38:05 -0300

    Add files via upload

---
+++ aula7.py
@@ -0,0 +1,21 @@
+# nome = input('digite seu nome')
+# idade = input('digite sua idade')
+# curso = input('digite seu curso')
+# numero = int(input('digite um numero:'))
+
+
+# if numero % 2 == 1:
+#     print('é divisivel')
+# elif numero> 100:
+#     print('é amior que 100')
+#     else:
+#      print('aqui nao tem nada')
+
+
+1
+
+wysdisclevou = int(input('digite um numero'))
+if wysdisclevou == 197:
+    print('veriique se o numero é positivo')
+else: wysdisclevou > 197:
+    print() 
